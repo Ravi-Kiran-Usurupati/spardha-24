b2ad0a675a4e81e4575833245d79b75bd356df66
Ravi kiran <167022620+Ravi-Kiran-Usurupati@users.noreply.github.com>
2025-09-13 15:16:58 +0530

    Update settings.py

--- backend/Spardha/Spardha/settings.py
+++ backend/Spardha/Spardha/settings.py
@@ -202,17 +202,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-STATICFILES_STORAGE = 'Spardha.storage.IgnoreMissingFilesStorage'
-
-# WHITENOISE_KEEP_ONLY_HASHED_FILES = True
-
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
-
-
-# # Activate Django-Heroku.
-# django_heroku.settings(locals())
-
-
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
